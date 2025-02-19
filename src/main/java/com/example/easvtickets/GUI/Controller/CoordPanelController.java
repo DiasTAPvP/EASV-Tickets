ccfe3814bbc055431de3bcd270869af2dcd82fcb
DiasTA <christofferabraham@gmail.com>
2025-02-19 14:33:56 +0100

    Changes by Bruger

--- src/main/java/com/example/easvtickets/GUI/Controller/CoordPanelController.java
+++ src/main/java/com/example/easvtickets/GUI/Controller/CoordPanelController.java
@@ -1,4 +1,19 @@
 package com.example.easvtickets.GUI.Controller;
 
 public class CoordPanelController {
+
+    private CoordScreenController setCoordScreenController;
+
+    public void setCoordScreenController(CoordScreenController coordScreenController) { {this.setCoordScreenController = coordScreenController;}
+    }
+
+    /**Implement:
+     * Table refreshes for the user/coordinator tables
+     * Some sort of listener to display the info of whatever event was selected
+     * The ability to edit the info of the selected event
+     * The ability to select a user and assign them as collaborators of your chosen event
+     * Delete button functionality
+     * Save button functionality
+     */
+
 }
--- src/main/java/com/example/easvtickets/GUI/Controller/NewEventController.java
+++ src/main/java/com/example/easvtickets/GUI/Controller/NewEventController.java
@@ -1,4 +1,32 @@
 package com.example.easvtickets.GUI.Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class NewEventController {
-}
+
+    /**Implement:
+     * Functionality for creating a new event and adding it to the database
+     * Error displays
+     * The ability to add event info when you create the event
+     * Save button (= create event)
+     * Adding custom ticket types to the database and
+     * reflecting those in the Ticket Generator for a given event
+     * Functionality for adding a special ("free") ticket type to an event
+     * **/
+
+
+    private CoordScreenController setCoordScreenController;
+
+
+    public void setCoordScreenController(CoordScreenController coordScreenController) {this.setCoordScreenController = coordScreenController;}
+    }
+
+
+
